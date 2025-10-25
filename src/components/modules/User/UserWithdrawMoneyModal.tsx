12d77e2d3b83f3f890e9d4335ad9f4599e734140
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-10-25 12:02:46 +0600

    convert text to number user withdraw

--- src/components/modules/User/UserWithdrawMoneyModal.tsx
+++ src/components/modules/User/UserWithdrawMoneyModal.tsx
@@ -168,10 +168,14 @@ export function UserWithdrawMoneyModal() {
                     <FormLabel>Amount</FormLabel>
                     <FormControl>
                       <Input
-                        type="number"
+                        type="text"
                         placeholder="Amount"
                         {...field}
                         value={field.value || ""}
+                        onChange={(e) => {
+                          const val = e.target.value;
+                          field.onChange(val === "" ? "" : Number(val));
+                        }}
                       />
                     </FormControl>
                     <FormMessage />
