5319a9f237709078bc36bdea34f51be436c50c53
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-10-25 12:09:49 +0600

    convert text to number user send money

--- src/components/modules/User/UserSendMoneyModal.tsx
+++ src/components/modules/User/UserSendMoneyModal.tsx
@@ -161,10 +161,14 @@ export function UserSendMoneyModal() {
                     <FormLabel>Amount</FormLabel>
                     <FormControl>
                       <Input
-                        type="number"
+                        type="text"
                         placeholder="Amount"
                         {...field}
                         value={field.value || ""}
+                        onChange={(e) => {
+                          const val = e.target.value;
+                          field.onChange(val === "" ? "" : Number(val));
+                        }}
                       />
                     </FormControl>
                     <FormMessage />
