4be58bf4b9305d190c46562ac1d006f9b771f30d
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-10-31 18:36:16 +0600

    title added

--- src/pages/User/UserCompleteTransactionTable.tsx
+++ src/pages/User/UserCompleteTransactionTable.tsx
@@ -69,7 +69,7 @@ const UserCompleteTransactionTable = () => {
       <div className="overflow-x-auto">
         <div className="bg-green-100 dark:bg-green-900 p-4 sm:p-6 rounded shadow text-center mt-2">
           <h2 className="text-lg sm:text-2xl font-bold text-green-700 dark:text-green-300">
-            Most recent Completed Transactions
+            Most Recent Completed Transactions
           </h2>
         </div>
 
--- src/pages/User/UserPendingTransactionTable.tsx
+++ src/pages/User/UserPendingTransactionTable.tsx
@@ -99,7 +99,7 @@ const UserPendingTransactionTable = () => {
       <div className="overflow-x-auto">
         <div className="bg-green-100 dark:bg-green-900 p-4 sm:p-6 rounded shadow text-center">
           <h2 className="text-lg sm:text-2xl font-bold text-green-700 dark:text-green-300">
-            Most recent Pending Transactions
+            Most Recent Pending Transactions
           </h2>
         </div>
 
